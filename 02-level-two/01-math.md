45c3df002eea8775bf69b346ba26c9a2e9e2e19d
Raj <asrajendrayadav@gmail.com>
2025-06-10 23:01:53 +0530

    resources added

--- 02-level-two/01-math.md
+++ 02-level-two/01-math.md
@@ -1,1 +1,19 @@
 ## Math functions
+
+> requires <cmath> library
+
+1. max and min funcion 
+
+```cpp
+
+cout << max(5, 10);
+cout << min(90, 3);
+
+```
+
+2. sqrt()
+3. rount()
+4. log()
+
+
+There are so many other useful functions in the math library.
---
+++ 02-level-two/02-conditionals.cpp
@@ -0,0 +1,23 @@
+// Let's explore the syntax of conditionals here..
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int a = 8, b = 9;
+
+  if (a > b){
+    cout << a << " is greater than " << b << endl;
+  }else{
+    cout << b << " is greater than " << a << endl;
+  }
+
+  // OP: 9 is greater than 8
+
+  // ?: -> ternary operator
+
+  
+
+  return 0;
+}
